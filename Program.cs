f1a77cbb8ec2056988e7f42e4abd26e7a47f1eb6
icmosoftware <ozendeniz@gmail.com>
2023-01-04 23:25:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Methodlar instance= new Methodlar();
+
+int a=10; int b=20;
+int result;
+
+instance.Topla(a,b,out result);
+Console.WriteLine(result);
+
+int ifade=999;
+instance.EkranaYazdir(ifade);
+
+class Methodlar{
+
+    public void Topla (int deger1, int deger2, out int toplam){
+
+        toplam= deger1 + deger2;
+    }
+
+    public void EkranaYazdir (string veri){
+
+        Console.WriteLine(veri);
+    }
+
+    public void EkranaYazdir (int veri){
+
+        Console.WriteLine(veri);
+    }
+
+
+}
+
+
